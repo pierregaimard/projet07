41d3e2c7c1c4eeec3486f1e2ee4a112173815ceb
Pierre Gaimard <pierre@gaimard-family.fr>
2021-02-10 18:56:58 +0100

    Remove users relation in Customer Entity

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -24,16 +24,6 @@ class Customer
      */
     private $name;
 
-    /**
-     * @ORM\OneToMany(targetEntity=User::class, mappedBy="customer", orphanRemoval=true)
-     */
-    private $users;
-
-    public function __construct()
-    {
-        $this->users = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -47,36 +37,6 @@ class Customer
     public function setName(string $name): self
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|User[]
-     */
-    public function getUsers(): Collection
-    {
-        return $this->users;
-    }
-
-    public function addUser(User $user): self
-    {
-        if (!$this->users->contains($user)) {
-            $this->users[] = $user;
-            $user->setCustomer($this);
-        }
-
-        return $this;
-    }
-
-    public function removeUser(User $user): self
-    {
-        if ($this->users->removeElement($user)) {
-            // set the owning side to null (unless already changed)
-            if ($user->getCustomer() === $this) {
-                $user->setCustomer(null);
-            }
-        }
 
         return $this;
     }
