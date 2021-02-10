7a8f5979ff864a6ec69a5024ce6fb9e5b8736e3f
Pierre Gaimard <pierre@gaimard-family.fr>
2021-02-10 19:01:10 +0100

    Fix code style in CustomerFixtures

--- src/DataFixtures/CustomerFixtures.php
+++ src/DataFixtures/CustomerFixtures.php
@@ -8,7 +8,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class CustomerFixtures extends Fixture
 {
-    public const CUSTOMER_FLEXCON = 'Flexcon';
+    public const CUSTOMER_FLEXCON       = 'Flexcon';
     public const CUSTOMER_BEST_PLATFORM = 'BestPlatform';
 
     public function load(ObjectManager $manager)
