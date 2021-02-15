c0b7c300a88e20788f1c5abfa450c940ec882636
Pierre Gaimard <pierre@gaimard-family.fr>
2021-02-15 16:25:50 +0100

    Use CustomErrorResponseManager service in LoginChecker

--- src/EventListener/LoginChecker.php
+++ src/EventListener/LoginChecker.php
@@ -2,8 +2,8 @@
 
 namespace App\EventListener;
 
+use App\Service\Response\CustomErrorResponseManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Serializer\Encoder\DecoderInterface;
@@ -11,7 +11,8 @@ use Symfony\Component\Serializer\Encoder\DecoderInterface;
 class LoginChecker implements EventSubscriberInterface
 {
     public function __construct(
-        private DecoderInterface $decoder
+        private DecoderInterface $decoder,
+        private CustomErrorResponseManager $errorResponseManager
     ){}
 
     public static function getSubscribedEvents()
@@ -29,16 +30,29 @@ class LoginChecker implements EventSubscriberInterface
             return;
         }
 
+        $statusCode = 400;
+        $type = $this->errorResponseManager->getErrorType($event->getRequest()->headers->get('accept'));
+
+        if ($type === false) {
+            return;
+        }
+
         # Missing Content-Type: application/json
         if ($event->getRequest()->headers->get('Content-Type') !== 'application/json') {
-            $event->setResponse($this->getFormattedResponse('Content-Type must be set to \'application/json\''));
+            $event->setResponse($this->errorResponseManager->getFormattedErrorResponse(
+                'Content-Type must be set to \'application/json\'',
+                $statusCode,
+                $type
+            ));
             return;
         }
 
         # Missing body authentication data
         if ($event->getRequest()->getContent(false) === "") {
-            $event->setResponse($this->getFormattedResponse(
-                'Request body is empty. Did you miss to provide username and password ?'
+            $event->setResponse($this->errorResponseManager->getFormattedErrorResponse(
+                'Request body is empty. Did you miss to provide username and password ?',
+                $statusCode,
+                $type
             ));
             return;
         }
@@ -47,36 +61,22 @@ class LoginChecker implements EventSubscriberInterface
 
         # Credentials keys are missing in request body
         if (!array_key_exists('username', $body) || !array_key_exists('password', $body)) {
-            $event->setResponse($this->getFormattedResponse(
-                'Invalid request body. You must provide \'username\' and \'password\' keys'
+            $event->setResponse($this->errorResponseManager->getFormattedErrorResponse(
+                'Invalid request body. You must provide \'username\' and \'password\' keys',
+                $statusCode,
+                $type
             ));
             return;
         }
 
         # Credentials informations are missing
         if ($body['username'] === "" || $body['password'] === "") {
-            $event->setResponse($this->getFormattedResponse(
-                'Invalid request body. You must provide values for \'username\' and \'password\' keys'
+            $event->setResponse($this->errorResponseManager->getFormattedErrorResponse(
+                'Invalid request body. You must provide values for \'username\' and \'password\' keys',
+                $statusCode,
+                $type
             ));
             return;
         }
-    }
-
-    /**
-     * @param string $message
-     *
-     * @return JsonResponse
-     */
-    private function getFormattedResponse(string $message): JsonResponse
-    {
-        return new JsonResponse(
-            [
-                'type' => 'https://tools.ietf.org/html/rfc2616#section-10',
-                'title' => 'An error occurred',
-                'status' => '400',
-                'detail' => $message
-            ],
-            400
-        );
     }
 }
